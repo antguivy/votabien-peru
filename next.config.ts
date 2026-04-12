f824ea5e91f1123f1975fd14ee150cdad0e88378
Anthony Villazana <63469220+antguivy@users.noreply.github.com>
2026-04-12 11:45:33 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,18 +15,6 @@ const withSerwist = withSerwistInit({
 const nextConfig: NextConfig = {
   // Para Docker/Dokploy
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/stats/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/api/stats/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
   skipTrailingSlashRedirect: true,
   images: {
     // Optimización de Next.js desactivada en producción
